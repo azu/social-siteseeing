3b74daec3f12f2a01b5f1f5c2454fd90b8d0d857
azu <azuciao@gmail.com>
2012-10-10 00:02:41 +0900

    はてなブックマークのコメントを表示する機能を追加

---
+++ README.md
@@ -0,0 +1,6 @@
+# What is?
+
+はてなブックマークやTwitterのコメントなどを見られる予定のSBMカウンタ的なもの
+
+
+ [Add-on SDK](http://www.asukaze.net/etc/jetpack/ "Add-on SDK") を使って書かれています
---
+++ data/contentScript.js
@@ -0,0 +1,44 @@
+/**
+ * Created by azu.
+ * Date: 12/10/08 19:22
+ * License: MIT License
+ */
+self.port.on("seeing", function(data){
+    //テンプレートの取得
+    var source = document.querySelector("#entry-template").innerHTML;
+    //テンプレートのコンパイル
+    var template = Handlebars.compile(source, function(){
+    });
+    //テンプレートに渡すパラメータ
+    var context = data;
+
+    //独自のヘルパーファンクションを登録
+    Handlebars.registerHelper('fullName', function(person){
+        return person.firstName + " " + person.lastName;
+    });
+    Handlebars.registerHelper('agree_button', function(){
+        return new Handlebars.SafeString("<button>I agree. I "
+                + this.emotion + " " + this.name + "</button>");
+    });
+    Handlebars.registerHelper('list', function(items, options){
+        var out = "<ul>";
+
+        for (var i = 0, l = items.length; i < l; i++){
+            out = out + "<li>" + options.fn(items[i]) + "</li>";
+        }
+
+        return out + "</ul>";
+    });
+    //テンプレートとパラメータをマージ
+    var result = template(context);
+
+    //結果を出力
+    document.querySelector(data.insertElementSelector).innerHTML = result;
+});
+self.port.on("clear", function(data){
+    var siteDiv = document.querySelectorAll("#contents > div");
+    for (var i = 0, len = siteDiv.length; i < len; i++){
+        var div = siteDiv[i];
+        div.innerHTML = "";
+    }
+})
