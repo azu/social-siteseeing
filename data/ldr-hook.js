836001606ec539bc0a9976004cb3212866aacf44
azu <azuciao@gmail.com>
2013-01-03 02:25:18 +0900

    少し柔軟にhookできるように

--- data/ldr-hook.js
+++ data/ldr-hook.js
@@ -4,6 +4,7 @@
  * License: MIT License
  */
 (function(){
+    // PageMod content
     window.addEventListener("CONTENT_social_site_seeing_message", function(request){
         var res = request.data;
         if (res === "hide"){
@@ -13,43 +14,40 @@
             self.postMessage(res);
         }
     }, false);
-    // mainhook
 
-
+    // main hook - execute in content page
     function evalInPage(func, args){
         var argStr = JSON.stringify(args || []);
         window.location.href = "javascript:void " + func + ".apply(null," + argStr + ")";
     }
+    var bootstrap = function(){
+        hookMain();
+        function hookMain(){
+            window.Keybind.add("m", function(){
+                var item = window.get_active_item(true);
+                if (item){
+                    var permalink = item.link.replace(/#/, '%23');
+                    pingToChrome(permalink);
+                }
+            });
+        }
 
-    window.addEventListener("load", function(){
-        evalInPage(function(){
-            if (document.readyState === "complete"){
-                hookMain();
-            }else{
-                window.addEventListener("load", function(){
-                    hookMain();
-                }, false);
-            }
-            function hookMain(){
-                window.Keybind.add("m", function(){
-                    var item = window.get_active_item(true);
-                    if (item){
-                        var permalink = item.link.replace(/#/, '%23');
-                        pingToChrome(permalink);
-                    }
-                });
-            }
-
-            /*Chrome領域へ通知*/
-            function pingToChrome(message){
-                /* Content -> PageMod */
-                var request = document.createEvent("MessageEvent");
-                request.initMessageEvent("CONTENT_social_site_seeing_message", true, false,
-                        message,
-                        location.protocol + "//" + location.host,
-                        "", window);
-                document.dispatchEvent(request);
-            }
-        }, []);
-    }, false);
+        /*Chrome領域へ通知*/
+        function pingToChrome(message){
+            /* Content -> PageMod */
+            var request = document.createEvent("MessageEvent");
+            request.initMessageEvent("CONTENT_social_site_seeing_message", true, false,
+                    message,
+                    location.protocol + "//" + location.host,
+                    "", window);
+            document.dispatchEvent(request);
+        }
+    };
+    if (document.readyState === "complete"){
+        evalInPage(bootstrap, []);
+    }else{
+        window.addEventListener("load", function(){
+            evalInPage(bootstrap, []);
+        }, false);
+    }
 })();
