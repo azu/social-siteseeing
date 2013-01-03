95ddecbfd6e9e57beba70d28f4aee56f59832d4d
azu <azuciao@gmail.com>
2013-01-03 17:15:39 +0900

    コメントをリンク化するように

--- data/contentScript.js
+++ data/contentScript.js
@@ -23,6 +23,13 @@ self.port.on(panel.seeing, function(data){
     emptyElem(insertElem);
     //テンプレートの取得
     var source = document.querySelector("#entry-template").innerHTML;
+    // autolink
+    Handlebars.registerHelper('auto_link_comment', function(text){
+        var escapedComment = Handlebars.Utils.escapeExpression(text);
+        return new Handlebars.SafeString(
+                window.autolinkTwitter(escapedComment)
+        );
+    });
     //テンプレートのコンパイル
     var template = Handlebars.compile(source, function(){
     });
@@ -56,3 +63,51 @@ function emptyElem(element){
         element.removeChild(element.firstChild);
     }
 }
+
+// https://github.com/tokuhirom/autolink.js
+// http://tokuhirom.mit-license.org/
+(function(){
+    "use strict";
+
+    var global = this;
+    var Y;
+    if (typeof exports !== 'undefined'){
+        Y = exports;
+    }else{
+        Y = window;
+    }
+
+    function escapeHTML(str){
+        return str.replace(/&/g, "&amp;").replace(/"/g, "&quot;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/'/g, '&#39;');
+    }
+
+    Y.autolink = function(src){
+        return src.replace(
+                /(https?:\/\/[^:/<>&\s]+(?::\d+)?(?:\/[^#\s<>&()"']*(?:#(?:[^\s<>&"'()]+))?)?)|(.)/gi,
+                function(all, url, normal){
+                    if (url){
+                        return "<a href='" + escapeHTML(url) + "'>" + escapeHTML(url) + "</a>"
+                    }else{
+                        return escapeHTML(normal);
+                    }
+                }
+        );
+    };
+    Y.autolinkTwitter = function(src){
+        return src.replace(
+                /(https?:\/\/[^:/<>&\s]+(?::\d+)?(?:\/[^#\s<>&()"']*(?:#(?:[^<>&"'()]+))?)?)|(?:@([a-zA-Z0-9_-]+))|(#[A-Za-z0-9_-]+)|(.)/gi,
+                function(all, url, name, hashtag, normal){
+                    if (url){
+                        return "<a href='" + escapeHTML(url) + "'>" + escapeHTML(url) + "</a>"
+                    }else if (name){
+                        return "<a href='http://twitter.com/" + escapeHTML(name) + "'>@" + name + "</a>";
+                    }else if (hashtag){
+                        return "<a href='https://twitter.com/#!/search/?q=" + encodeURIComponent(hashtag) + "'>" + escapeHTML(hashtag) + '</a>';
+                    }else{
+                        return escapeHTML(normal);
+                    }
+                }
+        );
+    }
+})();
+
